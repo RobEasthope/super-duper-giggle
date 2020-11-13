d4067eab729a3668a134b91b6a07973d69fb41c5
Rob Easthope <hello@robeasthope.com>
2020-11-13 13:41:46 +0000

    Add test serverless function

---
+++ api/getLocationData.js
@@ -0,0 +1,8 @@
+export default async (req, res) => {
+  try {
+    const locationData = await fetch('https://www.metaweather.com/api/location/search/?lattlong=36.96,-122.02');
+    res.status(200).json(locationData);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
