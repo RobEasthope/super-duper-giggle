0791429f71bda7e30171f19e4679c7cf697660a7
Rob Easthope <hello@robeasthope.com>
2020-11-13 13:56:48 +0000

    Log to check for errors

--- api/getLocationData.js
+++ api/getLocationData.js
@@ -1,9 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { fetchApiData} from '../src/utils/fetchApiData';
+
 export default async (req, res) => {
-  // try {
-  //   const locationData = await fetch('https://www.metaweather.com/api/location/search/?lattlong=36.96,-122.02');
-  //   res.status(200).json(locationData);
-  // } catch (error) {
-  //   res.status(500).json({ error });
-  // }
-  res.send(`Hello!`)
+  try {
+    const locationData = await fetchApiData('https://www.metaweather.com/api/location/search/?lattlong=36.96,-122.02');
+    res.status(200).json(locationData);
+    console.log(JSON.parse(locationData));
+
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
