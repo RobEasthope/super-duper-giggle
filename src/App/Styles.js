fdb348f1b8584fbee9cd4be84b5ff135f9eff237
Rob Easthope <hello@robeasthope.com>
2020-11-13 15:49:00 +0000

    Set styles based on temperature

--- src/App/Styles.js
+++ src/App/Styles.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
 
+function bkgColour(temperature) {
+    if(temperature <= -10){
+      return '#00ffff';} else if (temperature === 10)
+      {return '#fff700';} else if(temperature >= 30){
+      return '#ff8c00';} else {
+    
+      return 'white'}
+}
+
 export const Styles = styled.div`
+  background-color: ${props => bkgColour(props.temperature)};
+
   .wrapper {
     min-height: 100vh;
     max-width: 40em;
