4c9d6c144cabafd7f80a34a59908866ffa3db250
Rob Easthope <hello@robeasthope.com>
2020-11-13 13:47:00 +0000

    Test lambda function is working

--- api/getLocationData.js
+++ api/getLocationData.js
@@ -1,8 +1,9 @@
 export default async (req, res) => {
-  try {
-    const locationData = await fetch('https://www.metaweather.com/api/location/search/?lattlong=36.96,-122.02');
-    res.status(200).json(locationData);
-  } catch (error) {
-    res.status(500).json({ error });
-  }
+  // try {
+  //   const locationData = await fetch('https://www.metaweather.com/api/location/search/?lattlong=36.96,-122.02');
+  //   res.status(200).json(locationData);
+  // } catch (error) {
+  //   res.status(500).json({ error });
+  // }
+  res.send(`Hello!`)
 };
